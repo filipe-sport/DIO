5010ee5ec484987a251e0d46a8beb4f04b22a366
Filipe <filipecesarufpe@gmail.com>
2022-11-30 09:47:05 -0300

    update

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("sucesso")
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch(err => console.error(err));
+  
+  
+  
