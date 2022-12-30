bf8611a6eba32f5a2489adf23842f01c7410fdfc
Filipe Cesar <106756536+filipe-sport@users.noreply.github.com>
2022-12-29 23:31:54 -0300

    upadate

---
+++ JAVA/tipos-variaveis/operadores.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+public class operadores {
+    //classe Operadores.java
+    String nome = "Filipe";
+    int idade = 38;
+    double peso = 92.9;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    Date dataNascimento = new Date();
+
+    // Opepradores aritméticos [+ adição, - subtração, * multiplicação, / divisão ]
+    // classe Operadores.java
+    double soma = 10.5 +15.7;
+    int subtração = 113 - 25;
+    int multiplicação = 20 * 2;
+    int divisão = 15 / 3;
+    int moodulo = 12 % 2;
+    double resultado = (10 * 3) + (20 / 4);
+    
+
+}
