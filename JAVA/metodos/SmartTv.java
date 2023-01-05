c83b3296409af8bee1049a14fade64fbdb312628
Filipe Cesar <106756536+filipe-sport@users.noreply.github.com>
2023-01-05 18:20:51 -0300

    update files

--- JAVA/metodos/SmartTv.java
+++ JAVA/metodos/SmartTv.java
@@ -1,5 +1,3 @@
-package metodos;
-
 public class SmartTv {
     
         boolean ligada = false;
--- JAVA/metodos/Usuario.java
+++ JAVA/metodos/Usuario.java
@@ -1,5 +1,3 @@
-package metodos;
-
 public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
